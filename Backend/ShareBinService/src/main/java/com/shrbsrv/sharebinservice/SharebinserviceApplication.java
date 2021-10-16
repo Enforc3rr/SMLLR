acfc5ac06026c7b0ecfc537c0417e1193795b28d
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-10-17 01:10:28 +0530

    Connected Key generation server and Main SmllR server

---
+++ Backend/ShareBinService/src/main/java/com/shrbsrv/sharebinservice/SharebinserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shrbsrv.sharebinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebinserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharebinserviceApplication.class, args);
+    }
+
+}
---
+++ Backend/ShareBinService/src/test/java/com/shrbsrv/sharebinservice/SharebinserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrbsrv.sharebinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharebinserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
