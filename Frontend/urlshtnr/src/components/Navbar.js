4dc147cd4cd12c64d90e76ebd214a7ff4bc2b39b
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-12-08 05:23:28 +0530

    Removed Account Button and changed the place of Sharebin and Smllr Button.

--- Frontend/urlshtnr/src/components/Navbar.js
+++ Frontend/urlshtnr/src/components/Navbar.js
@@ -2,21 +2,24 @@ import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import "../App.css";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light shadow-5-strong">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/sb" alt="">
-          <u>ShareBin</u>
-        </a>
-        <a className="navbar-brand" href="/" alt="">
-          <u>SMLLR</u>
-        </a>
-        <form className="form-inline my-2 my-lg-0">
-          <button className="btn btn-outline-success my-2 my-sm-0">
-            Account
+        <a className="navbar-brand navbarTitle" href="/sb" alt="">
+          <button className="btn btn-outline-dark  my-2 my-sm-0">
+            <u>Sharebin</u>
           </button>
+        </a>
+
+        <form className="form-inline my-2 my-lg-0">
+          <a className="navbar-brand navbarTitle" href="/" alt="">
+            <button className="btn btn-outline-dark my-2 my-sm-0">
+              <u>SMLLR</u>
+            </button>
+          </a>
         </form>
       </div>
     </nav>
